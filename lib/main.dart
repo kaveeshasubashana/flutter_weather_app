4d1d5b7cbda6c61eb2d9dab50a7e81a60143dcf7
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-10-28 17:09:46 +0530

    set wether factory

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/consts.dart';
+import 'package:weather/weather.dart';
 
 void main() {
   runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+    final WeatherFactory _wf =WeatherFactory(OPENWETHER_API_KEY);
+
+    Weather? _weather;
+    
+    @override
+  void initState() {
+    super.initState();
+    _wf.currentWeatherByCityName("collombo").then(
+      (w){
+          setState(() {
+            _weather = w;
+          });
+      }
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp();
   }
 }
